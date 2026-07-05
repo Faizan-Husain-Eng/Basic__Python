f12dc0154ce1e9f84b57c65f59313f0c608632ae
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-05 16:57:31 +0530

    comments

--- comments2.py
+++ comments2.py
@@ -1,1 +1,9 @@
-print(" hello am very good boy. from bareilly Am studying in KMCLU, B.tech CSE with AI&ML")
+# hello am learing now python 
+#  author: Faizan 
+#  course: python
+print(" hello am very\' good boy. from \"bareilly\"\Am studying/in KMCLU, B.tech CSE with AI&ML")
+
+# print(" Hello World ") #single line comment#
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Herry")
