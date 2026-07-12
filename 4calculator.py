d2473348b10d0c56b45080a7f557292f42031ffd
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-12 17:01:18 +0530

    calculatoir

---
+++ 4calculator.py
@@ -0,0 +1,29 @@
+a = 13 
+b = 15 
+ #addition 
+ans1 = a+b
+print("Addition of",a,"and","is", ans1) 
+
+ #subtraction
+ans2 = a-b
+print("Subtraction of",a,"and","is", ans2)
+
+ #multiplication 
+ans3 = a*b 
+print("Multiplicaton of",a,"and","is", ans3) 
+
+ #division 
+ans4 = a/b 
+print("Division of",a,"and","is", ans4) 
+
+ #exponential 
+ans5 = a**b
+print("Exponential of",a,"and","is", ans5)
+
+ #floor division 
+ans6 = a//b
+print("Floor Division of",a,"and","is", ans6)
+ 
+ #modulas
+ans7 = a%b
+print("Modulas of",a,"and","is", ans7) 
