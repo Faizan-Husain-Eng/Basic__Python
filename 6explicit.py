d00eb0ba9f208a40ab33f975db603c8cb5fa5df9
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-13 16:39:32 +0530

    explicit-typecasting

---
+++ 6explicit.py
@@ -0,0 +1,7 @@
+string = "15"
+number = 7
+string_number = int(string)
+
+#thrpws an error if the string is not a valid integer
+sum = number + string_number
+print("The Sum of both the number is:" ,sum)
