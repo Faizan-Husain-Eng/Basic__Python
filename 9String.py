238c496772be7ee41c068805ee824837caf6e636
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-16 17:17:18 +0530

    String

---
+++ 9String.py
@@ -0,0 +1,25 @@
+name = "Faizan"
+friend = "Faiz"
+anotherfriend = "Abbas"
+ 
+apple = """ hello am good and you... 
+hey am faizan 
+you know " am from
+bareilly """
+
+print("Assalamo alaikum: ", name)   
+#print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+#print(name[6]) throws an error
+
+#loop
+print("lets use a for loop\n")
+
+for character in friend:
+    print(character)
