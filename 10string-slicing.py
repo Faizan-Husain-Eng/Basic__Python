5298543827366b3b8a1671d4b7acf6894b593da4
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-19 17:03:04 +0530

    slicing

---
+++ 10string-slicing.py
@@ -0,0 +1,13 @@
+# fruit = "banana"
+# bananaLen = len(fruit)
+
+# # print(faizanLen)
+# # print(man[0:5]) #including 0
+# # print(man[1:4]) #including 1
+# # print(man[:5])
+# print(fruit[0:len(fruit)-4])
+# print(fruit[-3:-2])
+
+#Quick Quiz]
+nm = "Harry"
+print(nm[-4:-2])
