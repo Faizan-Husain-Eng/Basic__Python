ceb8709134190920684043eb542f2f2f2b289997
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-23 17:11:51 +0530

    elif

---
+++ 14elif.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the value of num: "))
+if(num < 0):
+    print("Number is negative.")
+
+elif (num == 0):
+    print("Number is Zero.")
+    
+elif (num == 110):
+    print("Number is Favorite: = 110")
+
+else:
+    print("number is positive.")        
+
+print("I am faizan who is doing the programming")    
