63fc956e5b69a2f8d94da1210f9a97fca1f12164
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-07-24 17:15:40 +0530

    loops

---
+++ 17while-loop.py
@@ -0,0 +1,26 @@
+# while loop 
+# i = 0
+# while(i <=3 ):
+#     print(i)
+#     i = i + 1
+
+
+
+  #While loop with userinput
+# i = int(input("Enter you number: "))
+# while(i<=38):
+#     i = int(input("Enter you number: "))
+#     print(i)
+
+# print("Done the while loop")  
+
+
+
+# with the count
+count = 5 
+while(count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("Hey: am Syed Faizan Husain Zaidi ")
+
